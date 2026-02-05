80a51195d5c4ec5c5ebdbe6cda57bf92f04eb861
DARSHAN K <darshanshyla23@gmail.com>
2026-02-05 10:09:52 +0530

    Replace all

---
+++ LogicsPractice/src/LOGICS/replaceall.java
@@ -0,0 +1,14 @@
+package LOGICS;
+
+public class replaceall {
+public static void main(String[] args) {
+	String str="javafa232563vsjnhcgsnbmasd876";
+	str=str.replaceAll("[^aeiou]","");
+	System.out.println(str);
+//	str=str.replaceAll("[0-9]","");
+//	System.out.println(str);
+//	str=str.replaceAll("[]","");
+//	System.out.println(str);
+	
+}
+}
