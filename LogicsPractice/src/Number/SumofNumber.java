2fd63af16b35326e68f9068d0d74fdc5c3fd9ab5
DARSHAN K <darshanshyla23@gmail.com>
2026-02-05 11:05:39 +0530

    Sum of number

---
+++ LogicsPractice/src/Number/SumofNumber.java
@@ -0,0 +1,14 @@
+package Number;
+
+public class SumofNumber {
+public static void main(String[] args) {
+	int no= 64;
+	int sum=0;
+	while(no!=0) {
+		int rem = no%10;
+		sum=sum+(rem*rem);
+		no=no/10;
+	}
+	System.out.println(sum);
+}
+}
