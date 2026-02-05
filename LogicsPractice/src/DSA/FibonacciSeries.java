1ef197c7b140f61bd62eb8fdd0bfe8ef6ab383d8
DARSHAN K <darshanshyla23@gmail.com>
2026-02-05 17:58:11 +0530

    Fibonacci

---
+++ LogicsPractice/src/DSA/FibonacciSeries.java
@@ -0,0 +1,18 @@
+package DSA;
+
+public class FibonacciSeries {
+	public static int fib(int no) {
+		if(no==0) {
+			return 0;		
+		}
+		else if(no==1||no==2) {
+			return 1;
+		}
+		else {
+			return fib(no-1)+fib(no-2);
+		}
+	}
+public static void main(String[] args) {
+	System.out.println(fib(7));
+}
+}
